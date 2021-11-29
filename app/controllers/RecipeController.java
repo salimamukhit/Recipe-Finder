408419e95c46177f127deed1515a6586a00fd9a1
Salima Mukhit <smukhit@dons.usfca.edu>
2021-11-28 19:59:19 -0800

    changed parameterization

--- app/controllers/RecipeController.java
+++ app/controllers/RecipeController.java
@@ -2,7 +2,9 @@ package controllers;
 
 import models.Ingredient;
 import models.Recipe;
+import play.api.mvc.Request;
 import play.db.jpa.JPAApi;
+import play.mvc.Http;
 import play.mvc.Result;
 import play.libs.Json;
 import services.RecipeService;
@@ -29,15 +31,19 @@ public class RecipeController {
         });
     }
 
-    public Result getRecipeList(String ingredients) {
+    public Result getRecipeList(Http.Request request) {
         return jpaApi.withTransaction(em -> {
             List<Recipe> result = null;
+
+            String ingredients = request.queryString().get("ingredients")[0];
+            if (ingredients.isEmpty()) {
+                return new Result(400);
+            }
 
             // Prepare a string for SQL
             String[] query = ingredients.split(",");
             String queryStr = "";
             for (String str : query) {
-                str = str.replaceAll("\\+", " ");
                 str = str.replaceAll("'", "''"); // An escape character for quotes
                 String s = "'" + str + "'" + ",";
                 queryStr += s;
